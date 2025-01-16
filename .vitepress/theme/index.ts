9cbe7717a3d391825972b02edf72fc9d83e6e443
whl <whlit.cola@gmail.com>
2025-01-16 23:10:23 +0800

    feat: home page background image

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Mermaid from '../components/Mermaid.vue'
+import './custom.css'
 
 export default <Theme>{
   extends: DefaultTheme,
